ddac2cb06b1bb121038f058a38fbcbfe06ad4c05
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-09 18:47:18 -0400

    Added more modules and mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
+import mongoose from 'mongoose'
+import bodyparser from 'body-parser'
 
 const app = express()
 const PORT = 8080
+const uri = process.env.MONGO_URI
+console.log(uri)
 
+mongoose.Promise = global.Promise
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).catch(error => handleError(error));
+
+app.use(bodyparser.urlencoded({extended:true}))
 app.get('/', (req, res) => 
     res.send(`Naked React - MERN:${PORT}`)
 )
 
-app.listen(PORT,() => console.log('SERVER STARTED'))
+app.listen(PORT,() => 
+    console.log('SERVER STARTED')
+)
